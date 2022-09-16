ab79b1667877484f164ae0d3b9d301b6efabf5e7
Laxmi Sarki <laxmi17sarki@gmail.com>
2022-09-17 00:54:05 +0530

    deploy

--- Forecast_trends/app.py
+++ Forecast_trends/app.py
@@ -34,4 +34,5 @@ def future_price_predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Calls the run method, runs the app on port 5000
+    app.run(host='0.0.0.0', port='5000')
